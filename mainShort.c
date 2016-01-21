75266c39476372efea48d3687496f35a3abf98c0
Tim Visée <timvisee@gmail.com>
2016-01-21 16:37:24 +0100

    Replaced for-loop with simplified version

--- mainShort.c
+++ mainShort.c
@@ -10,38 +10,11 @@ int evaluateCircuit(int a, int b, int c, int d);
 
 int main() {
     int max = 16;
-    int i;
 
     printf("1 2 3 4   output\n");
 
-    for(i = 0; i < max; i++) {
-        int pos8 = 0,
-                pos4 = 0,
-                pos2 = 0,
-                pos1 = 0,
-                x = i;
-
-        if(x >= 8) {
-            pos8 = 1;
-            x -= 8;
-        }
-
-        if(x >= 4) {
-            pos4 = 1;
-            x -= 4;
-        }
-
-        if(x >= 2) {
-            pos2 = 1;
-            x -= 2;
-        }
-
-        if(x == 1) {
-            pos1 = 1;
-        }
-
-        displayLine(pos8, pos4, pos2, pos1);
-    }
+    for(int i = 0; i < max; i++)
+        displayLine(i & 8 ? 1 : 0, i & 4 ? 1 : 0, i & 2 ? 1 : 0, i & 1);
 
     return 0;
 }
